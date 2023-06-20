10031092f4db139c9509bdd29fb8a83d9dc564fa
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-20 15:27:55 +0300

    Created C File

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - function definition
+ * @c: character ascii code
+ * Return:(1: character is lowercase, 0: otherwise)
+ */
+int _islower(int c)
+{
+	if (c >= 97)
+		return (1);
+
+	return (0);
+}
