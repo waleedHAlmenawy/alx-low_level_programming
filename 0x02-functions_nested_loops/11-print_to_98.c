44b1aebb5aef39751dcdbb278a3197ea4fe08c81
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-20 19:45:57 +0300

    Created C File

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include "_putchar.h"
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n < 0)
+		{	int r = n * -1;
+			_putchar('-');
+			if ((r / 100) != 0)
+				_putchar(48 + (r / 100));
+			if ((r / 10) != 0)
+				_putchar(48 + ((r / 10) % 10));
+			_putchar(48 + (r % 10));
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+
+		if (n == 0)
+		{
+			_putchar(48);
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+
+		if (n > 0)
+		{
+			if ((n / 100) != 0)
+				_putchar(48 + (n / 100));
+			if ((n / 10) != 0)
+				_putchar(48 + ((n / 10) % 10));
+			_putchar(48 + (n % 10));
+			_putchar(',');
+			_putchar(' ');
+			if (n < 98)
+				n++;
+			n--;
+		}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
+
+int main()
+{
+	print_to_98(177);
+}
