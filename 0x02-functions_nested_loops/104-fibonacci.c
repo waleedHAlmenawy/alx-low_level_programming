d9066fa25d52b162fb058464cafcb81d4a61c2e8
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-21 13:00:45 +0300

    Edited C File

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,6 +3,24 @@
  * main - entry point
  * Return: 0
  */
+
+void print(long n)
+{
+    // If number is smaller than 0, put a - sign
+    // and change number to positive
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
+
+    // Remove the last digit and recur
+    if (n/10)
+        print(n/10);
+
+    // Print the last digit
+    putchar(n%10 + '0');
+}
+
 int main(void)
 {
 	int i;
@@ -11,16 +29,18 @@ int main(void)
 	num1 = 0;
 	num2 = 1;
 	num3 = 0;
-
+	
 	for (i = 0; i < 98; i++)
 	{
 		num3 = num1 + num2;
 		if (i == 97)
 		{
-			printf("%ld\n", num3);
+			print(num3);
+			printf("\n");
 		} else
 		{
-			printf("%ld, ", num3);
+			print(num3);
+			printf(", ");
 		}
 
 		num1 = num2;
