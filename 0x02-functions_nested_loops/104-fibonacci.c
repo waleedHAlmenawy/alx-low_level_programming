46f2f84559851961fe2fb8ec52bdeb108e4e0140
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-21 12:27:24 +0300

    Created C File

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long num1, num2, num3;
+
+	num1 = 0;
+	num2 = 1;
+	num3 = 0;
+
+	for (i = 0; i < 98; i++)
+	{
+		num3 = num1 + num2;
+		if (i == 97)
+		{
+			printf("%ld\n", num3);
+		} else
+		{
+			printf("%ld, ", num3);
+		}
+
+		num1 = num2;
+		num2 = num3;
+
+	}
+	return (0);
+}
