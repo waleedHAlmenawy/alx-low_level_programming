d4cb3ebe62239192955d4e23cd60f4f247c510b8
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-21 16:45:22 +0300

    Edited C FIle

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -13,9 +13,9 @@ int main(void)
     int day;
     int year;
 
-    month = 2;
-    day = 28;
-    year = 2001;
+    month = 4;
+    day = 01;
+    year = 2000;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
