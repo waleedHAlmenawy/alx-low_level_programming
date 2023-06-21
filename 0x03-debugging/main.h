7a413232d06c1d0f1be4d632d890833f9bad5522
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-21 16:22:40 +0300

    Created C File

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -13,3 +13,5 @@ void print_to_98(int n);
 void print_times_table(int n);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
