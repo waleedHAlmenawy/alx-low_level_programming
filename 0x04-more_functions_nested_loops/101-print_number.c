5dec9a5057b4e32bdccad1dccf6f2e5b7516a405
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-22 16:37:28 +0300

    Created C File

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,16 +5,18 @@
  */
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = -num
 	}
 
-	if ((n / 10) > 0)
+	if ((num / 10) > 0)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
 
-	_putchar(48 + (n % 10));
+	_putchar(48 + (num % 10));
 }
