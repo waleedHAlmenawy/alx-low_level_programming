ce3cde6c7b0f011dab0291833bafe79b0f20771f
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-22 12:09:06 +0300

    Edited C File

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,9 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i == 50 || i == 52)
+		if (i == 2 || i == 4)
 			continue;
-		_putchar('0' + i)
+		_putchar('0' + i);
 	}
 
 	_putchar('\n');
