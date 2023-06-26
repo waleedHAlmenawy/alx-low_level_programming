ba84463963dbe261e7b87a7cb1eeee8bf3bbe4aa
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-26 20:10:29 +0300

    Create C File

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - function definition
+ * @str: String
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != 0)
+	{
+		len++;
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		if (i % 2 == 0)
+			_putchar('str[i]');
+	}
+
+	_putchar(10);
+}
