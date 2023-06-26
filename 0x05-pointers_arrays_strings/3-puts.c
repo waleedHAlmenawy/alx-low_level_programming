527cd305c33fcfd92f24b9de8c85593bf4658c45
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-26 16:01:12 +0300

    Create C File

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: String
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	-putchar(10);
+}
