1afe2db538b66bbba12549cc4bd04d4d0d0c1cfa
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-26 16:38:06 +0300

    Create C File

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,13 @@ void rev_string(char *s)
 {
 	int i, k, j;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s != '\0'; i++)
 	{}
 	j = i;
 
 	for (k = 0; k < i; k++)
 	{
-		*s[k] = *s[j];
+		s[k] = *s[j];
 		j--;
 	}
 }
