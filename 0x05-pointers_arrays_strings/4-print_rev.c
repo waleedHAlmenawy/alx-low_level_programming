20e5840bf980728a16ceb2189655d192f48f0690
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-26 16:11:07 +0300

    Create C File

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: String
+ */
+
+void print_rev(char *s)
+{
+	int i, k, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	j = i;
+
+	for (k = 0; k <= i; k++)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+}
