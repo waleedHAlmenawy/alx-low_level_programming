eb18c5eddabc87692d96011101787961d08f6c23
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-26 15:45:58 +0300

    Create C File

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - count string length
+ * @s: String
+ * Return: @i String Length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s[i];
+	}
+	return (i);
+}
