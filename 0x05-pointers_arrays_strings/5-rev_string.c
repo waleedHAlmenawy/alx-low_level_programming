e51a28e8324020889fbae70c31bda61409328bc8
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-26 16:32:02 +0300

    Create C File

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverse String
+ * @s: String
+ */
+void rev_string(char *s)
+{
+	int i, k, j;
+
+	for (i = 0; i != '\0'; i++)
+	{}
+	i = j;
+
+	for (k = 0; k < i; k++)
+	{
+		s[j] = *s + k;
+		j--;
+	}
+}
