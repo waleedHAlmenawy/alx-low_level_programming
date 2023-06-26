b79654d2d551c49ebad44357bc94bfa602cb15bf
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-26 23:57:28 +0300

    Edit C File

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _atoi - argument to intger
+ * _atoi - argument to integer
  * @s: pointer
  * Return: 0
  */
@@ -14,7 +14,7 @@ int _atoi(char *s)
 		if (s[i] >= 48 && s[i] <= 57)
 		{
 			if (s[i - 1] == '-')
-				sign = -1;
+				sign *= -1;
 			num = num * 10 + (s[i] - 48);
 		}
 		if ((s[i] < 48 || s[i] > 57) && num != 0)
