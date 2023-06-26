6258c57a569cba2e7b0267d96bdd0f1549e0f851
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-26 20:27:45 +0300

    Create C File

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - function definition
+ * @str: String
+ */
+void puts_half(char *str);
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != 0)
+	{
+		len++;
+	}
+
+	if (len / 2 != 0)
+		len--;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i >= (len / 2))
+			_putchar(str[i]);
+	}
+
+	_putchar(10);
+}
