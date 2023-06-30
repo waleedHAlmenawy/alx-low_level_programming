7a8b6218dd54b07f62a8728bbada5217c9c3db7f
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-30 22:27:12 +0300

    Create C File

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,17 +5,19 @@
  */
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
-		n = -n;
+		num = -num;
 		_putchar('-');
 	}
-	if (n >= 10)
+	if ((num / 10) > 0)
 	{
-		print_number(n / 10);
-		_putchar((n % 10) + 48);
+		print_number(num / 10);
+		_putchar((num % 10) + 48);
 	} else
 	{
-		_putchar((n % 10) + 48);
+		_putchar((num % 10) + 48);
 	}
 }
