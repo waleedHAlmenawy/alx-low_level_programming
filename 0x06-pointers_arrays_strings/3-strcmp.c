0214072524758ce6cc76430491a30a21e9b5c705
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-30 03:47:33 +0300

    Edit C File

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,20 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, count;
 
 	i = 0;
+	count = 0;
 
 	while (s1[i] != 0 || s2[i] != 0)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			count = s1[i] - s2[i];
+			break;
 		}
 		i++;
 	}
+
+	return (count);
 }
