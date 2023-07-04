14a9043c3015d553ec4e2508ef43af56bc567d40
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-04 04:26:24 +0300

    Create C File

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: String
+ * @accept: Matching Characters
+ * Return: @s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, k, check;
+
+	i = 0, k = 0, check = 0;
+	while (s[i] != 0)
+	{
+		while (accept[k] != 0)
+		{
+			if (s[i] == accept[k])
+			{
+				s += i;
+				check = 1;
+				break;
+			}
+			k++;
+		}
+		if (check)
+			break;
+		i++, k = 0;
+	}
+
+	return (s);
+}
