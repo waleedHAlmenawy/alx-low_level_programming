d04376bbe21a5dd8260c9bc5954ab115dfad74f4
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-04 04:55:15 +0300

    Create C File

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strstr - function definition
+ * @haystack: String
+ * @needle: String
+ * Return: 0 if null, @haystack if not null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, k, check;
+
+	i = 0, k = 0, check = 0;
+	while (haystack[i] != 0)
+	{
+		while (needle[k] != 0)
+		{
+			if (haystack[i + k] == needle[k])
+				check++;
+			k++;
+		}
+		if (check == k)
+		{
+			haystack += i;
+			break;
+		}
+		i++, k = 0, check = 0;
+	}
+
+	if (check)
+		return (haystack);
+	return (0);
+}
