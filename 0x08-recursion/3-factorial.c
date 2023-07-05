fa241c221f9f92fdde271a1e13d558bbcb6caf19
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-05 20:09:32 +0300

    Create C File

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - function definition
+ * @n: Integer
+ * Return: Integer
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return n * factorial(n - 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 int _strlen_recursion(char *s);
+int factorial(int n);
 void _puts_recursion(char *s);
