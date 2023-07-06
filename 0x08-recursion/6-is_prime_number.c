9f73cb2dcb0246882fb9c638a634542d56cdc44b
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-06 05:58:00 +0300

    Create C File

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * helper - function definition
+ * @n: Int
+ * @fac: Int
+ *
+ * Return: 1 if it's prime, other wise 0
+ */
+
+int helper(int n, int fac)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	if (fac == 1)
+	{
+		return (1);
+	}
+
+	if (n % fac == 0)
+	{
+		return (0);
+	}
+
+	return (helper(n, fact - 1));
+}
+
+/**
+ * is_prime_number - Check if a number is a prime number
+ * @n: Int
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	return (helper(n, n / 2));
+}
