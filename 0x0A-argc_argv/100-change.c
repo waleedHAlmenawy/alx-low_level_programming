e6d676438b3f8603c362f8bb4dcc4ae0e1bd168f
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-07 18:51:43 +0300

    Edit C File

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,35 +1,48 @@
-#include <stdio.h>
 #include <stdlib.h>
 /**
  * main - entry point
  * @argc: Number of arguments passed in the command line
- * @argv: array of arguments passed in the command line
- * Return: 0 if success, otherwise 1
+ * @argv: Number of arguments passed in the command line
+ * Return: 0 when success
  */
 int main(int argc, char *argv[])
 {
-	int money = atoi(argv[1]), change = 0, coins[5] = {25, 10, 5, 2, 1}, i = 0;
+	int amt_of_m = atoi(argv[1]), change = 0;
 
 	if (argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (money < 0)
+	if (amt_of_m < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	while (i < 5)
+	if (amt_of_m >= 25)
 	{
-		if (money > coins[i] || coins[i] == 1)
-		{
-			change += money / coins[i];
-			money -= (money / coins[i]) * coins[i];
-		}
-		i++;
+		change = amt_of_m / 25;
+		amt_of_m -= 25 * (amt_of_m / 25);
 	}
-
+	if (amt_of_m >= 10 && amt_of_m < 25)
+	{
+		change += amt_of_m / 10;
+		amt_of_m -= 10 * (amt_of_m / 10);
+	}
+	if (amt_of_m >= 5 && amt_of_m < 10)
+	{
+		change += amt_of_m / 5;
+		amt_of_m -= 5 * (amt_of_m / 5);
+	}
+	if (amt_of_m >= 2 && amt_of_m < 5)
+	{
+		change += amt_of_m / 2;
+		amt_of_m -= 2 * (amt_of_m / 2);
+	}
+	if (amt_of_m == 1)
+	{
+		change += 1;
+	}
 	printf("%d\n", change);
 	return (0);
 }
