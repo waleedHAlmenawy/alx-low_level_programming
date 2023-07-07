3acc4e69c4f32aff3aff6a074325a4a5ccb00f65
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-07 17:15:22 +0300

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: Number of arguments passed in the command line
+ * @argv: array of arguments passed in the command line
+ * Return: 0 if success, otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, count = 0;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+	} else if (argc > 1)
+	{
+		while (i < argc)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			count += atoi(argv[i]);
+			i++;
+		}
+	}
+	printf("%d\n", count);
+	return (0);
+}
