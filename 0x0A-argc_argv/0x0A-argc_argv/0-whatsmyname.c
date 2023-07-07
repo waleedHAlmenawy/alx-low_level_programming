09f4958780a5ae654b0adfc4c5721289b431efd8
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-07 16:03:22 +0300

    Create C File

---
+++ 0x0A-argc_argv/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: counts the arguments that supplied to the program
+ * @argv: an array of pointers to the strings which are those arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
