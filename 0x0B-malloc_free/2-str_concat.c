74129fbe0794134aa53467fe7693ca0b2937f651
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-10 17:07:55 +0300

    Create C File

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+/**
+ * _strlen - counts the length of a string
+ * @s: A string
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (1);
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * str_concat - concatenates two strings
+ * @s1: First string
+ * @s2: Second String
+ * Return: A pointer to a string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len, i = 0, k = 0;
+char *heap;
+
+	len = _strlen(s1) + _strlen(s2);
+	heap = (char *) malloc(sizeof(char) * (len));
+
+	if (heap == NULL)
+		return (NULL);
+
+	while (s1[i] != 0)
+	{
+		heap[i] = s1[i];
+		i++;
+	}
+
+	while (s2[k] != 0)
+	{
+		heap[i] = s2[k];
+		i++, k++;
+	}
+	return (heap);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,4 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int _putchar(char c);
+char *str_concat(char *s1, char *s2);
