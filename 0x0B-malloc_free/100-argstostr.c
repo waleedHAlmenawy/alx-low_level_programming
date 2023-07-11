8c8c070152ef1a5bb70291e5941c47c422f9ed36
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-11 18:10:42 +0300

    Create C File

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+/**
+ * strlen - counts the length of a string
+ * @s: A string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: Number of arguments
+ * @av: arguments
+ * Return: A pointer to an array of arguments
+ */
+char *argstostr(int ac, char **av)
+{
+	int slen = 0, i = 0, k = 0, j = 0;
+	char *coStr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (k < ac)
+	{
+		slen += _strlen(av[k]);
+		k++;
+	}
+	coStr = (char *)malloc(sizeof(char) * (slen + ac));
+	if (coStr == NULL)
+		return (NULL);
+	k = 0;
+	while (i < ac)
+	{
+		while (k < _strlen(av[i]))
+		{
+			coStr[j] = av[i][k];
+			k++, j++;
+		}
+		coStr[j] = 10;
+		i++, j++, k = 0;
+	}
+	return (coStr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,3 +4,4 @@ int _putchar(char c);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
