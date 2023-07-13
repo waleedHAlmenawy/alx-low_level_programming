dba35e3c4c4fda373d3426b62b32f43248f4a764
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-13 13:13:54 +0300

    Create C File

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,7 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= lens2)
 		n = lens2;
 
-	while (i < lens1 && s[i] != 0)
+	while (i < lens1 && *s1 != 0)
 	{
 		s[i] = s1[i];
 		i++;
