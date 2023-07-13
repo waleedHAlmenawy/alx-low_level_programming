e823d456689794db9e6413a2a8152d2e3ccc40b1
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-13 13:01:23 +0300

    Create C File

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+/**
+ * _strlen - string length
+ * @s: A string
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of bytes
+ * Return: A string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int lens1, lens2, i = 0, k = 0;
+	char *s;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL && s2 != NULL)
+		*s1 = 0;
+	if (s2 == NULL && s1 != NULL)
+		*s2 = 0;
+
+	lens1 = _strlen(s1);
+	lens2 = _strlen(s2);
+
+	s = malloc((lens1 + lens2) * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	if (n >= lens2)
+		n = lens2;
+
+	while (i < lens1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	while (k < n)
+	{
+		s[i] = s2[k];
+		k++, i++;
+	}
+	return (s);
+}
