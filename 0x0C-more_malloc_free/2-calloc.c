017b0808a9b7359e358d08287a7472afed3d6776
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-13 15:41:36 +0300

    Create C File

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+/**
+ * _calloc - reserve an array in heap
+ * @nmemb: Array's elements
+ * @size: Size of each elements in bytes
+ * Return: A pointer to an array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0, k = 0;
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	while (i < nmemb)
+	{
+		p = malloc(size);
+		if (p == NULL)
+		{
+			k = i;
+			while (k != 0)
+			{
+				free(p);
+				k--;
+			}
+			free(p);
+			return (NULL);
+		}
+		p++, i++;
+	}
+	p -= (nmemb - 1);
+	i = 0;
+	while (i < nmemb)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
