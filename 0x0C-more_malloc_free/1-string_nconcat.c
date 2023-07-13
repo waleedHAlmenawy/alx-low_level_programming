64b2a89cef1c93832b8826588855ddb24048b05d
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-13 13:04:14 +0300

    Create C File

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,11 +27,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+		return ("");
 	if (s1 == NULL && s2 != NULL)
-		*s1 = 0;
+		s1 = "";
 	if (s2 == NULL && s1 != NULL)
-		*s2 = 0;
+		s2 = "";
 
 	lens1 = _strlen(s1);
 	lens2 = _strlen(s2);
