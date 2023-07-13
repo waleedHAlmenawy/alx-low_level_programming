61640fc3027f3454fc2eb5e06f6f6e155cb7833d
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-13 13:33:07 +0300

    Create C File

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,20 +1,5 @@
 #include <stdlib.h>
 /**
- * _strlen - string length
- * @s: A string
- * Return: The length of a string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != 0)
-	{
-		i++;
-	}
-	return (i);
-}
-/**
  * string_nconcat - concatenates two strings
  * @s1: First string
  * @s2: Second string
@@ -23,7 +8,7 @@ int _strlen(char *s)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int lens1, lens2, i = 0, k = 0;
+	unsigned int i = 0, k = 0;
 	char *s;
 
 	if (s1 == NULL && s2 != NULL)
@@ -33,10 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL && s2 == NULL)
 		s1 = "", s2 = "";
 
-	lens1 = _strlen(s1);
-	lens2 = _strlen(s2);
-
-	s = malloc((lens1 + lens2 + 1) * sizeof (char));
+	s = malloc((lens1 + lens2 + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
@@ -44,12 +26,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= lens2)
 		n = lens2;
 
-	while (i < lens1 && *s1 != 0)
+	while (s1[i] != 0)
 	{
 		s[i] = s1[i];
 		i++;
 	}
-	while (k < n && n != 0)
+	while (k < n && s2[k] != 0)
 	{
 		s[i] = s2[k];
 		k++, i++;
