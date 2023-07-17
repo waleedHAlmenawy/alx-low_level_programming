292032c1d17ef3357333481ed58f6e42b9f8b40e
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-17 21:37:33 +0300

    Edit header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,6 +8,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		struct dog my_dog;
+	d = &my_dog;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
