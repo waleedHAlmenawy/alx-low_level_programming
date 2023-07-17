87930010b6873e3ec70c8a238ef7fac998f683cd
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-17 20:55:13 +0300

    Create C File

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: A pointer to struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
