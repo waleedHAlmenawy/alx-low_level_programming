838dfb468fc939f72ba2dac2fe35af51c72a5c6f
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-18 02:54:11 +0300

    Create C File

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,10 +1,11 @@
 #include "dog.h"
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t my_dog;
+	dog_t new_dog;
+	dog_t *my_dog = &new_dog;
 
-	my_dog.name = name;
-	my_dog.age = age;
-	my_dog.owner = owner;
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
 	return (my_dog);
 }
