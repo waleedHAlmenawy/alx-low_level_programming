6b5fec14be05a91c7ff4ad9e7f23ca9aead1f013
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-20 02:54:23 +0300

    Create C File

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * array_iterator - executes a fun given as a parameter on each element of an array
+ * @array: An array
+ * @size: Number of elements
+ * @action: Fuction to be executed
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (array != NULL && action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
