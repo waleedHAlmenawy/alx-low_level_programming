19dc7f2d3855c36c004caf60a0f7560193a32972
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-20 07:47:00 +0300

    Edit C Files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,22 +12,22 @@ int main(int argc, char *argv[])
 	int result, fNum, sNum;
 	int (*op)(int, int);
 
-	fNum = atoi(argv[1]), sNum = atoi(argv[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((sNum == 0 && *argv[2] == '/') || (sNum == 0 && *argv[2] == '%'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+	fNum = atoi(argv[1]), sNum = atoi(argv[3]);
 	op = get_op_func(argv[2]);
 	if (op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
+	}
+	if ((sNum == 0 && *argv[2] == '/') || (sNum == 0 && *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	result = (*op)(fNum, sNum);
 
