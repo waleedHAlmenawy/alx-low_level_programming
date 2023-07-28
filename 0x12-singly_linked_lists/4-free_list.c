92844996ccef86e33dde00d3ae1e98fcc71e7659
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-28 06:01:31 +0300

    Create free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * free_list - function that frees a list_t list
@@ -7,14 +8,14 @@
 
 void free_list(list_t *head)
 {
-	list_t *ptr = head;
-	list_t *next;
+	list_t *node = head;
+	list_t *temp;
 
-	while (ptr != NULL)
+	while (node != NULL)
 	{
-		next = ptr->next;
-		free(ptr->str);
-		free(ptr);
-		ptr = next;
+		temp = node->next;
+		free(node->str);
+		free(node);
+		node = temp;
 	}
 }
