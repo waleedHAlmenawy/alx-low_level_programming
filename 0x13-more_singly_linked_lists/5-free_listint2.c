19ffa6ee60e1b5e600e413031deb663164590c13
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-31 19:49:42 +0300

    Edit C File

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,12 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	temp = *head;
-	while (temp != NULL)
+	while (temp->next != NULL)
 	{
 		*head = temp->next;
 		free(temp);
 		temp = *head;
 	}
+	free(temp);
 	*head = NULL;
 }
