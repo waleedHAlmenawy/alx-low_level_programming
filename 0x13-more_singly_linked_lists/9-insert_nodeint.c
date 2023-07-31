719ae92fb5a393cb09cb981d2757e071dd51b278
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-31 20:50:35 +0300

    Edit C File

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	temp = *head;
-	while (i < idx)
+	while (i < idx - 1)
 	{
 		temp = temp->next;
 		i++;
