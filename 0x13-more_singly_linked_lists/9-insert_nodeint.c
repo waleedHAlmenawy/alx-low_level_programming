15dfbb39ece8dd7dfa28da4bd350a8e96fd99c1c
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-31 21:10:57 +0300

    Edit C File

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,8 +25,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		i++;
 	}
-	new->next = temp->next;
-	temp->next = new;
+	if (idx != 0)
+	{
+		new->next = temp->next;
+		temp->next = new;
+	}
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
 
 	return (new);
 }
