33b714197a94e8ad3ed8f49947b20495d920d3cf
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-07-31 10:58:06 +0300

    Create 101-hello_holbertob.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+extern printf
+
+section .text
+	global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+
+section .data
+	text db "Hello, Holberton",10
