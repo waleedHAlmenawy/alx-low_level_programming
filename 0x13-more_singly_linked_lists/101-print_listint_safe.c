0569414c40b5ba65d03ee2253c05324e0c39ea9a
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-08-01 15:45:10 +0300

    Edit C File

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,7 +15,7 @@ size_t print_listint_safe(const listint_t *head)
 		exit(98);
 	while (head != NULL)
 	{
-		while (i < len)
+		while (i < len && head->next != NULL)
 		{
 			if (test == head)
 			{
