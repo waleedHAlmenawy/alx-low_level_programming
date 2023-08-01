57aab1be21623ce1801ed81d00e2f5290c357fff
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-08-01 11:19:38 +0300

    Edit C File

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,6 +10,8 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *temp = *head;
 	listint_t *rev;
 
+	if (!head || !*head)
+		return (NULL);
 	rev = *head;
 	*head = (*head)->next;
 	rev->next = NULL;
