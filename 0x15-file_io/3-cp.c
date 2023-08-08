107a3325d397b0f99addd6378bd9503ef6f83b04
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-08-08 15:30:42 +0300

    Edit cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,9 @@
 #include "main.h"
 #define READ_BUF_SIZE 1024
+#define MSG_USAGE "Usage: cp file_from file_to\n"
+#define MSG_ERR_NOREAD "Error: Can't read from file %s\n"
+#define MSG_ERR_NOWRITE "Error: Can't write to %s\n"
+#define MSG_ERR_NOCLOSE "Error: Can't close fd %d\n"
 /**
  * main - check the code
  *
@@ -15,35 +19,23 @@ int main(int ac, char **av)
 	ssize_t n_bytes;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
+		dprintf(STDERR_FILENO, MSG_USAGE), exit(97);
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	}
+		dprintf(STDERR_FILENO, MSG_ERR_NOREAD, av[1]), exit(98);
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
-	}
+		dprintf(STDERR_FILENO, MSG_ERR_NOWRITE, av[2]), exit(99);
 	while ((n_bytes = read(fd_from, buf, READ_BUF_SIZE)) > 0)
 	{
 		if (write(fd_to, buf, n_bytes) != n_bytes)
-		{
-			dprintf(2, "Error: Can't write to file %s\n", av[2]);
-			exit(99);
-		}
+			dprintf(2, MSG_ERR_NOWRITE, av[2]), exit(99);
 	}
 	if (n_bytes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, MSG_ERR_NOREAD, av[1]), exit(98);
 	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_from), exit(100);
+		dprintf(STDERR_FILENO, MSG_ERR_NOCLOSE, fd_from), exit(100);
 	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_to), exit(100);
+		dprintf(STDERR_FILENO, MSG_ERR_NOCLOSE, fd_to), exit(100);
 	return (0);
 }
