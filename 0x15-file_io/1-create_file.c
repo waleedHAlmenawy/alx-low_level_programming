6d9a2b163be6248fc1d4e3aa8971fabb9f4d1587
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-08-08 13:35:20 +0300

    Edit create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,6 +14,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
+	if (!text_context)
+		text_context = "";
 	if (write(fd, text_content, strlen(text_content)) == -1)
 		return (-1);
 	close(fd);
