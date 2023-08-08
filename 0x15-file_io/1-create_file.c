c90d92aa4b28d0d334835c5d79df261ff3d3a65e
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-08-08 13:33:08 +0300

    Create create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * create_file - creates a file with permissions
+ * @filename: The file name
+ * @text_content: The file's content
+ * Return: 1 when success otherwise -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	if (write(fd, text_content, strlen(text_content)) == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
